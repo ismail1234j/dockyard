b901babc11c41aebfd52a78d5707d0a214ffab18
10ij <git@ismailj.eu.org>
2026-02-17 10:09:57 +0000

    fix: was calling deleted file

--- src/users/toggle_force_reset.php
+++ src/users/toggle_force_reset.php
@@ -1,7 +1,6 @@
 <?php
 require_once '../includes/auth.php';
 require_once '../includes/functions.php';
-require_once '../includes/notifications.php';
 
 // Require admin privileges
 require_admin();
